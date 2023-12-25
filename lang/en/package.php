11bf5bec3a55f5da4c4bcaa7d07f2e4ffb655e89
Kostikas <k.kostikas93@gmail.com>
2023-12-25 21:57:05 +0200

    pinted 2

--- lang/en/package.php
+++ lang/en/package.php
@@ -14,7 +14,8 @@ return [
             'save_success' => 'Successfully saved',
             'delete_success' => 'Successfully deleted',
             'delete_fail' => 'Delete failed',
-            'delete_fail_protected_relationships' => 'You can’t delete <strong>“:model_name”</strong> because it’s associated with other records',        ],
+            'delete_fail_protected_relationships' => 'You can’t delete <strong>“:model_name”</strong> because it’s associated with other records',
+        ],
     ],
 
 ];
